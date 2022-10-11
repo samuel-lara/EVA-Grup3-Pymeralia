52eeb164aa07ea2cf230b3209ba27e0ccb2bceb1
Francisco Zahinos <franciscozahinos@iesmontsia.org>
2022-10-11 21:19:30 +0200

    curs.php por acabar

---
+++ pymeraliaFiles/clases/Curs.php
@@ -0,0 +1,20 @@
+<?php
+class Curs
+{
+    private $idCurso;
+    private $nombreCurso;
+    private $descripcionCurso;
+    private $imagenCurso;
+
+    private function addCurso() {
+    }
+    private function editCurso() {
+    }
+    private function deleteCurso() {
+    }
+    public function showCursos() {
+    }
+    private function enableCurso() {
+    }
+}
+?>
