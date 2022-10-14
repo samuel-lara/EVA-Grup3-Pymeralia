f6e27fe6ba9e9bb1132d410f478a71b89827d28d
samuel-lara <samuellara@iesmontsia.org>
2022-10-14 08:12:07 +0200

    class recursos, valoraciones done

--- pymeraliaFiles/clases/Recusos_class.php
+++ pymeraliaFiles/clases/Recusos_class.php
@@ -3,58 +3,117 @@ class Recursos{
     private $idRecurso;
     private $nombreRecurso;
 
-    /** Constructor de la clase Recursos */
+    /** Constructor de la clase Recursos */    
+    /**
+     * __construct
+     *
+     * @param  mixed $idRecurso
+     * @param  mixed $nombreRecurso
+     * @return void
+     */
     public function __construct($idRecurso, $nombreRecurso){
-        $this->idRecurso = $idRecurso
-        $this->nombreRecurso = $nombreRecurso
+        $this->idRecurso = $idRecurso;
+        $this->nombreRecurso = $nombreRecurso;
     }
 
-    /** getter Id Recurso */
+    /** getter Id Recurso */    
+    /**
+     * getIdRecurso
+     *
+     * @return void
+     */
     public function getIdRecurso(){
         return $this->idRecurso;
     }
 
-    /** getter Nombre Recurso */
+    /** getter Nombre Recurso */    
+    /**
+     * getNombreRecurso
+     *
+     * @return void
+     */
     public function getNombreRecurso(){
         return $this->nombreRecurso;
     }
 
-    /** setter Id Recurso */
+    /** setter Id Recurso */    
+    /**
+     * setIdRecurso
+     *
+     * @param  mixed $idRecurso
+     * @return void
+     */
     private function setIdRecurso($idRecurso){
-        $this->idRecurso = $idRecurso
+        $this->idRecurso = $idRecurso;
     }
 
-    /** setter Nombre Recurso */
+    /** setter Nombre Recurso */    
+    /**
+     * setNombreRecurso
+     *
+     * @param  mixed $nombreRecurso
+     * @return void
+     */
     private function setNombreRecurso($nombreRecurso){
-        $this->nombreRecurso = $nombreRecurso
+        $this->nombreRecurso = $nombreRecurso;
     }
 
-    /** Método que añade un recurso nuevo */
+    /** Método que añade un recurso nuevo */    
+    /**
+     * addRecursos
+     *
+     * @return void
+     */
     private function addRecursos(){
 
     }
 
-    /** Método que edita un recurso existente */
+    /** Método que edita un recurso existente */    
+    /**
+     * editRecursos
+     *
+     * @return void
+     */
     private function editRecursos(){
 
     }
 
-    /** Método que elimina un recurso existente */
+    /** Método que elimina un recurso existente */    
+    /**
+     * deleteRecursos
+     *
+     * @return void
+     */
     private function deleteRecursos(){
 
     }
 
-    /** Método que muestra un recurso existente */
+    /** Método que muestra un recurso existente */    
+    /**
+     * showRecursos
+     *
+     * @return void
+     */
     public function showRecursos(){
 
     }
 
-    /** Método que asigna un recurso */
+    /** Método que asigna un recurso */    
+    /**
+     * assignRecurso
+     *
+     * @return void
+     */
     private function assignRecurso(){
 
     }
 
-    /** Método que desasigna un recurso */
+    /** Método que desasigna un recurso */    
+    /**
+     * unassignRecurso
+     *
+     * @return void
+     */
     private function unassignRecurso(){
 
     }
