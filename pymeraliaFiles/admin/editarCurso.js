6c34e3393de4e4bd2e023cca70f1a83deb3ed452
PolObalat <93244671+PolObalat@users.noreply.github.com>
2022-10-17 17:13:14 +0200

    editarCursodelete

--- pymeraliaFiles/admin/editarCurso.js
+++
@@ -1,8 +0,0 @@
-function validarInputObligatorio(){
-    inputObligatorio = document.getElementsByClassName("form-control obligatorio")
-
-    if (inputObligatorio.length == 0 || /^\s+$/.test(inputObligatorio)) {
-        
-    }
-    return true;
-}
