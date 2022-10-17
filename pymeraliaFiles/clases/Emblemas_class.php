e297fc392f71e6495a35734cba07a58ee11d6a38
TatianaValentiny <94828921+TatianaValentiny@users.noreply.github.com>
2022-10-17 16:04:17 +0200

    Update Emblemas_class.php

--- pymeraliaFiles/clases/Emblemas_class.php
+++ pymeraliaFiles/clases/Emblemas_class.php
@@ -1,35 +1,178 @@
 <?php 
-include './connect.php';
 class Emblemas {
-    private $añadir;
-    private $editar;
-    private $eliminar;
-  // Methods
-  function construct($añadir, $editar, $eliminar) {
-    $this->añadir = $añadir;
-    $this->editar = $editar;
-    $this->eliminar = $eliminar;
+  private $idEmblema;
+  private $nombreEmblema;
+  private $descripcionEmblema;
+  private $imagenEmblema;
+
+  
+  /**
+   * __construct
+   *
+   * @param  mixed $idEmblema
+   * @param  mixed $nombreEmblema
+   * @param  mixed $descripcionEmblema
+   * @param  mixed $imagenEmblema
+   * @return void
+   */
+  public function __construct($idEmblema, $nombreEmblema, $descripcionEmblema, $imagenEmblema){
+    $this->idEmblema = $idEmblema;
+    $this->nombreEmlema = $nombreEmlema;
+    $this->descripcionEmblema = $descripcionEmblema;
+    $this->imagenEmblema = $imagenEmblema;
+  }
+  
+  /**
+   * getIdEmblema
+   *
+   * @return void
+   */
+  public function getIdEmblema(){
+    return $this->idEmlema;
+  }
+  
+  /**
+   * getNombreEmblema
+   *
+   * @return void
+   */
+  public function getNombreEmblema(){
+    return $this->nombreEmblema;
+  }
+  
+  /**
+   * getDescripcionEmblema
+   *
+   * @return void
+   */
+  public function getDescripcionEmblema(){
+    return $this->descripcionEmblema;
+  }
+  
+  /**
+   * getImagenEmblema
+   *
+   * @return void
+   */
+  public function getImagenEmblema(){
+    return $this->imagenEmblema;
   }
 
-  function get_añadir() {
-    return $this->añadir;
+  
+  /**
+   * setIdEmblema
+   *
+   * @param  mixed $idEmblema
+   * @return void
+   */
+  private function setIdEmblema($idEmblema){
+    $this->idEmblema = $idEmblema;
+  }
+  
+  /**
+   * setNombreEmblema
+   *
+   * @param  mixed $nombreEmblema
+   * @return void
+   */
+  private function setNombreEmblema($nombreEmblema){
+    $this->nombreEmblema = $nombreEmblema;
+  }
+  
+  /**
+   * setDescripcionEmblema
+   *
+   * @param  mixed $descripcionEmblema
+   * @return void
+   */
+  private function setDescripcionEmblema($descripcionEmblema){
+    $this->descripcionEmblema = $descripcionEmblema;
+  }
+  
+  /**
+   * setImagenEmblema
+   *
+   * @param  mixed $imagenEmblema
+   * @return void
+   */
+  private function setImagenEmblema($imagenEmblema){
+    $this->imagenEmblema = $imagenEmblema;
   }
 
-  function set_añadir() {
-    return $this->añadir;
-  }
-  function get_editar() {
-    return $this->editar;
-  }
-  function set_editar() {
-    return $this->editar;
-  }
-  function get_eliminar() {
-    return $this->eliminar;
-  }
-  function set_eliminar() {
-    return $this->eliminar;
-  }
+  
+  /**
+   * addEmblema
+   *
+   * @return void
+   */
+  private function addEmblema(){
+
 }
+  
+  /**
+   * editEmblema
+   *
+   * @return void
+   */
+  private function editEmblema(){
 
+}
+  
+  /**
+   * deleteEmblema
+   *
+   * @return void
+   */
+  private function deleteEmblema(){
+
+}
+  
+  /**
+   * showEmblema
+   *
+   * @return void
+   */
+  public function showEmblema(){
+
+}
+  
+  /**
+   * enableEmlema
+   *
+   * @return void
+   */
+  private function enableEmlema(){
+
+  }
+  
+  /**
+   * disableEmblema
+   *
+   * @return void
+   */
+  private function disableEmblema(){
+
+  }
+  
+  /**
+   * assingEmblema
+   *
+   * @return void
+   */
+  private function assingEmblema(){
+
+  }
+  
+  /**
+   * unassingEmlema
+   *
+   * @return void
+   */
+  private function unassingEmlema(){
+
+  }
+
+}
 ?>
+
+  
